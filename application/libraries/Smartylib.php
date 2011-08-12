e115b208154b16656e12854b2511dcf7de556cbb
Wayne Khan <kzhiwei@gmail.com>
2011-08-12 13:18:31 +0800

    Cleaning up.

--- application/libraries/Smartylib.php
+++ application/libraries/Smartylib.php
@@ -2,9 +2,9 @@
 require('Smarty-3.0.8/libs/Smarty.class.php');
 class Smartylib extends Smarty {
 	public function __construct() {
-		$this->compile_dir = FCPATH . APPPATH . 'cache'; // ./application/cache/templates_c
-		$this->template_dir = FCPATH . APPPATH . 'views' . DS; // ./application/views
-
+		$this->compile_dir = FCPATH . APPPATH . 'cache' . DS; // ./application/cache/
+		$this->template_dir = FCPATH . APPPATH . 'views' . DS; // ./application/views/
+		
 		$ci =& get_instance();
 		//print_r($ci); die; // debug
 		$ci->load->helper('url');
