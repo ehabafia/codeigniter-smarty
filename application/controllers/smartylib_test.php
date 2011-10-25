4195732255027c0430dde9e30169c3e51a0eccb0
Wayne Khan <kzhiwei@gmail.com>
2011-10-25 12:16:04 +0800

    Adding a second test.

--- application/controllers/smartylib_test.php
+++ application/controllers/smartylib_test.php
@@ -2,6 +2,17 @@
 class Smartylib_Test extends CI_Controller {
 	public function index() {
 		$this->load->library(array('unit_test', 'smartylib'));
-		echo $this->unit->run($this->smartylib, 'is_object', 'Smartylib_Test');
+	
+		// Test #1.
+
+		$this->unit->run($this->smartylib, 'is_object');
+
+		// Test #2.
+
+		$expected = 'bar';
+		$this->smartylib->assign('foo', $expected);
+		$actual = trim($this->smartylib->fetch('smartylib_test.tpl'));
+		$this->unit->run($expected, $actual);
+		echo $this->unit->report();
 	}
 }
