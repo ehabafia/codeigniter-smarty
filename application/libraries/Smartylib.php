35c93ebccfd2072df43ede7ac1c58b50d80a9e72
Wayne Khan <kzhiwei@gmail.com>
2011-10-25 11:32:04 +0800

    Cleaning things up a bit.

--- application/libraries/Smartylib.php
+++ application/libraries/Smartylib.php
@@ -1,13 +1,20 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 require('Smarty-3.0.8/libs/Smarty.class.php');
 class Smartylib extends Smarty {
 	public function __construct() {
-		$this->compile_dir = FCPATH . APPPATH . 'cache' . DS; // ./application/cache/
-		$this->template_dir = FCPATH . APPPATH . 'views' . DS; // ./application/views/
+		parent::__construct();
 		
+		// See http://www.smarty.net/docs/en/installing.smarty.extended.tpl.
+
+		$this->caching = Smarty::CACHING_LIFETIME_CURRENT;
+		$this->setCompileDir(FCPATH . APPPATH . 'cache' . DS);
+		$this->setCacheDir(FCPATH . APPPATH . 'cache' . DS);
+		$this->setTemplateDir(FCPATH . APPPATH . 'views' . DS);
+
+		// Assigning get_instance(), site_url() to Smarty.
+
 		$ci =& get_instance();
-		//print_r($ci); die; // debug
-		$ci->load->helper('url');
+		$ci->load->helper('url'); // Otherwise site_url() is fatal.
 		$this->assign('ci', $ci);
 		$this->assign('site_url', site_url());
 	}
