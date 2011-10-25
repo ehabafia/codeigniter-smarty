c63a3e88a0bb181f978a3182bc7917ccd0a5c1b3
Wayne Khan <kzhiwei@gmail.com>
2011-10-25 14:33:16 +0800

    Merged Smarty 3.0.8.

--- application/libraries/Smartylib.php
+++ application/libraries/Smartylib.php
@@ -1,21 +1,18 @@
 <?php
-require('Smarty-3.0.8/libs/Smarty.class.php');
+/**
+ * See http://www.smarty.net/docs/en/installing.smarty.extended.tpl.
+ */
+require './application/third_party/Smarty-3.0.8/libs/Smarty.class.php';
 class Smartylib extends Smarty {
 	public function __construct() {
 		parent::__construct();
 		
-		// See http://www.smarty.net/docs/en/installing.smarty.extended.tpl.
-
 		$this->caching = Smarty::CACHING_LIFETIME_CURRENT;
 		$this->setCompileDir(FCPATH . APPPATH . 'cache' . DS);
 		$this->setCacheDir(FCPATH . APPPATH . 'cache' . DS);
 		$this->setTemplateDir(FCPATH . APPPATH . 'views' . DS);
-
-		// Assigning get_instance(), site_url() to Smarty.
-
-		$ci =& get_instance();
-		$ci->load->helper('url'); // Otherwise site_url() is fatal.
-		$this->assign('ci', $ci);
-		$this->assign('site_url', site_url());
 	}
 }
+
+/* End of file Smartylib.php */
+/* Location: ./application/libraries/Smartylib.php */
